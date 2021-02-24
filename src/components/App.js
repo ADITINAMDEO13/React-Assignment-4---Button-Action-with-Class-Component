ee61c882aca11ddf793cbf0cbad8d5f7aa578b14
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-02-24 15:20:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,41 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-		super(props);
-	};
+    super(props);
+    this.state = {
+      paragraph: [{
+        paraState : false,
+      }],  
+    }
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleClick(){
+    
+    this.setState({
+      paragraph : [{
+        paraState : true,
+      }],
+    })
+    
+  }
 
     render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
+      return(
+        <div id="main">
+        { /* Do not remove this main div!! */ }
+        <button id="click" onClick={this.handleClick}>Click It</button>
+        {
+          this.state.paragraph[0].paraState ? 
+
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+          :
+          null
+        }
+        </div>
+      );
     }
 }
 
 
 export default App;
-
